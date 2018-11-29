e89daa277c7511a9aaa006407209690935cf4034
stefaan <dev@bluehome.biz>
2018-11-29 01:45:08 +0100

    enabled influx + fixed config to only capture non numeric topics

--- mqtt2influxdb.js
+++ mqtt2influxdb.js
@@ -2,7 +2,20 @@
 
 const Influx = require('influx');
 const mustache = require('mustache');
+const mqtt = require('mqtt');
+const { createLogger, format, transports } = require('winston');
 const config = require('./config.js').parse();
+
+// Initate the logger
+const logger = createLogger({
+    level: config.loglevel,
+    format: format.combine(
+      format.colorize(),
+      format.splat(),
+      format.simple(),
+    ),
+    transports: [new transports.Console()]
+});
 
 let parse = function(topic, message) {
     // ensure data is Object
@@ -27,7 +40,7 @@ let parse = function(topic, message) {
             if (field === 'value')
                 fields[field] = Number(fields[field]);
             }
-            console.log(fields);      
+            logger.verbose('Published %s, fields: %s, tags: %s', measurement, JSON.stringify(fields), JSON.stringify(tags));
         }
     }
 
@@ -37,12 +50,12 @@ let start = (new Date).getTime();
 parse('knx/status/a/b/c', JSON.stringify({'dst' : '0/0/108', 'value' : '10', 'label' : 'shit'}), config.topics["knx/status/+/+/+"]);
 console.log("time needed: " + ((new Date).getTime() - start));
 
-//const influx = new Influx.InfluxDB(config.influx);
+const influx = new Influx.InfluxDB(config.influx);
 
 /**
  * Now, we'll make sure the database exists and boot the app.
  */
-/*
+
 influx.getDatabaseNames()
     .then(names => {
       if (!names.includes('mqtt2influx')) {
@@ -50,13 +63,42 @@ influx.getDatabaseNames()
       }
     })
     .then(() => {
-        console.log('Influx DB ready to use.');
+        console.log('Influx DB ready to use. Connecting to MQTT.');
+        let mqttClient = mqtt.connect(config.mqtt.url, config.mqtt.options);
+        setMqttHandlers(mqttClient);
     })
     .catch(err => {
         console.error('Error creating Influx database!');
     }
 )
 
+let setMqttHandlers = function(mqttClient) {
+    mqttClient.on('connect', function () {
+        logger.info('MQTT connected');
+        for (const topic of config.topics) {
+            mqttClient.subscribe(topic);
+            logger.verbose('subscribed to %s', topic);
+        }
+    });
+
+    mqttClient.on('close', function () {
+        logger.info('MQTT disconnected');
+    });
+
+    mqttClient.on('reconnect', function () {
+        logger.info('MQTT trying to reconnect');
+    });
+
+    mqttClient.on('message', function (topic, message) {
+        // message is a buffer
+        message = message.toString();
+        console.log(topic + "  " + message);
+        parse(topic, message);
+    });
+
+}
+
+/*
 influx.writePoints([
     {
         measurement: 'response_times',
